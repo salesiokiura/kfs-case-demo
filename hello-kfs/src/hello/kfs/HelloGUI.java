014c24874a67b684dd721392d51814f0a3507c9d
salesiokiura <34238535+salesiokiura@users.noreply.github.com>
2018-01-25 16:27:15 +0300

    Running Hello World

---
+++ hello-kfs/src/hello/kfs/HelloGUI.java
@@ -0,0 +1,22 @@
+package hello.kfs;
+
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Salesio
+ */
+public class HelloGUI {
+    
+    void greetThem(){
+    JOptionPane.showMessageDialog(null, "Welcome to KFS");
+    
+    }
+}
